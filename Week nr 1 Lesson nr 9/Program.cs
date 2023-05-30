7c10ce16773c882759856c7a4ebbe11a3120939d
Ilka <Ilka@LAPTOP-VAVOB6QC>
2023-05-30 19:49:43 +0200

    Add project files.

---
+++ Week nr 1 Lesson nr 9/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Week_nr_1_Lesson_nr_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PascalCase dla klas, nazw plików, namespace, nazw metod");
+            Console.WriteLine("camelCase dla lokalnych zmiennych i dla elementów prywatnych");
+            Console.WriteLine("UPPER_CASE");
+            Console.Write("I jak interfejs");
+        }
+    }
+}
